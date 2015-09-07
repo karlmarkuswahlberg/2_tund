ae8e37dd3969cc5d33c31e88e29b7092fbedbc44
Karl Markus Wahlberg <skmw@greeny.cs.tlu.ee>
2015-09-07 13:28:28 +0300

    Added initials of my name

--- helloworld.php
+++ helloworld.php
@@ -1,1 +1,13 @@
-jee
+<?php
+
+//muutujaid märgitakse $ märgiga.
+//tühikuid ei saa kasutada, alakriips, väiketähed. semikoolon lõpp.
+
+//ei ole defaultina nähtav
+$first_name="Kmw";
+
+//trükib välja
+echo $first_name; 
+
+?>
+
