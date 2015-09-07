29ecbe415b066b609866c175a4f39019dd1ab7e4
Karl Markus Wahlberg <skmw@greeny.cs.tlu.ee>
2015-09-07 14:57:26 +0300

    user login page

---
+++ user_form.php
@@ -0,0 +1,23 @@
+<?php
+
+	//user_form.php
+
+?>
+<html>
+	<head>
+		<title>User login page</title>
+	</head>
+	
+	<body>
+		
+		<h2>Log in</h2>
+			<input type="email" placeholder="e-post"> <br><br>
+			<input type="password" placeholder="parool"> <br><br>
+			<input type="submit" value="Logi sisse">
+		<h2>Create user</h2>
+		
+		
+		
+	</body>
+
+</html>
